88a6d3d06c8836ce38a49d8b0a8ebc346537829b
Don Newman <dnewman@talentpath.com>
2019-11-12 14:53:37 -0600

    Calculator Functionality Tests

--- calc.test.js
+++ calc.test.js
@@ -34,3 +34,31 @@ describe("Calculator", () => {
       const subject = new Calculator(5).expect(subject.divide).toBeDefined();
     };
 });
+
+describe("Calculator Functionality", () => {
+  test.todo("Check 2 plus 3"),
+    () => {
+      expect(new Calculator(2).add(3)).toBe(5);
+    };
+});
+
+describe("Calculator Functionality", () => {
+  test.todo("Check 6 times 7"),
+    () => {
+      expect(new Calculator(6).multiply(7)).toBe(42);
+    };
+});
+
+describe("Calculator Functionality", () => {
+  test.todo("Check 44 minus 3"),
+    () => {
+      expect(new Calculator(44).subtract(3)).toBe(41);
+    };
+});
+
+describe("Calculator Functionality", () => {
+  test.todo("Check 666 divided by 1"),
+    () => {
+      expect(new Calculator(666).divide(1)).toBe(666);
+    };
+});
